4e691676d761efaf15b5164c93bac91f44a99ba4
Helias <stefanoborzi32@gmail.com>
2015-10-12 22:17:51 +0200

    Importing project

---
+++ utilities.py
@@ -0,0 +1,24 @@
+# Coded By Adriano Ferraguto (Telegram: @adriano_effe)
+# -*- coding: utf-8 -*-
+
+import json
+    
+def getProfessori(input):
+    with open("professori.json") as data_file:
+        professori_data = json.load(data_file)
+    output = ""
+    i = 0
+    risultati = 0
+    while(professori_data[i]["ID"] != "-1") :
+        if(input.lower() in professori_data[i]["Nome"].lower() or input.lower() in professori_data[i]["Cognome"].lower()):
+            output += "Ruolo: " + professori_data[i]["Ruolo"] + "\n"
+            output += "Cognome: " + professori_data[i]["Cognome"] + "\n"
+            output += "Nome: " + professori_data[i]["Nome"] + "\n"
+            output += "Indirizzo email : " + professori_data[i]["Email"] + "\n"
+            output += "Sito web: " + professori_data[i]["Sito"] + "\n"
+            output += "Scheda DMI: " + professori_data[i]["SchedaDMI"] + "\n\n"
+            risultati += 1
+        i += 1
+    if (risultati == 0) :
+        output = "\nNon sono stati trovati risultati :(\n\n"
+    return output
