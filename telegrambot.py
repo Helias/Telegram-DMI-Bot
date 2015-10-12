25043f51c086cbdf1130b608ede0e7ca3e4f529a
Adriano Ferraguto <0xADFE@gmail.com>
2015-10-12 23:34:50 +0200

    Updated the strings to correct punctualization in UTF-8
    Changed the strings printed by the bot to use punctualization following
    the UTF-8 standard codec.

--- telegrambot.py
+++ telegrambot.py
@@ -27,13 +27,13 @@ while True:
 		if (text == '/help' or text == '/help@dmi_bot'):
 			messageText = "@DMI_Bot risponde ai seguenti comandi (per la magistrale scrivete una \"m\" davanti ad ogni comando per esempio /mlezioni): \n/lezioni - /mlezioni - linka il calendario delle lezioni \n/esami - /mesami - linka il calendario degli esami  \n/aulario - linka l\'aulario \n/prof <nome> - restituisce una lista dettagliata di professori i cui nomi e/o cognomi contengano <nome> - es. /prof Milici \n\nSegreteria orari e contatti:\n/sdidattica - segreteria didattica \n/sstudenti - segreteria studenti \n ERSU orari e contatti \n/ersu - sede centrale\n/ufficioersu - (ufficio tesserini)\n\n/urp - URP studenti\n\n     Coded By @Helias && @adriano_effe"
 		elif (text == '/sdidattica' or text == '/sdidattica@dmi_bot'):
-			messageText = 'Sede presso il Dipartimento di Matematica e Informatica (primo piano vicino al laboratorio) \n\nSig.ra Cristina Mele Tel. 095/7337227\nEmail: cmele@dmi.unict.it\n\nOrari:\nLunedi\' dalle 15:00 alle 17:00\nGiovedi\' dalle 10:00 alle 12:00'
+			messageText = 'Sede presso il Dipartimento di Matematica e Informatica (primo piano vicino al laboratorio) \n\nSig.ra Cristina Mele Tel. 095/7337227\nEmail: cmele@dmi.unict.it\n\nOrari:\nLunedì dalle 15:00 alle 17:00\nGiovedì dalle 10:00 alle 12:00'
 		elif (text == '/sstudenti' or text == '/sstudenti@dmi_bot'):
 			messageText = 'Segreteria studenti\nSede presso la Cittadella Universitaria (vicino la mensa)\n\nVia S. Sofia, 64 ed. 11 - 95125 Catania\nTel. 095.7386103, 6119, 6109, 6125, 6129, 6123, 6122, 6106, 6107, 6121\nEmail: settore.scientifico@unict.it\n\nOrario invernale:\nLunedi\': 10:00 - 12.30\nMartedi\': 10:00 -12:30 | 15:00 - 16:30\nGiovedi\': 10:00 - 12:30 | 15:00 - 16:30\nVenerdi\': 10:00 - 12:30'
 		elif (text == '/ersu' or text == '/ersu@dmi_bot'):
-			messageText = 'ERSU Catania - sede centrale\nSede presso Via Etnea, 570\nTel. 095/7517940 (ore 9:00/12:00)\nEmail: urp@ersucatania.gov.it\n\nOrari:\nLunedi\': 09:00 - 12:00\nMercoledi\': 15:30 - 18:00\nVenerdi\': 09:00 - 12:00'
+			messageText = 'ERSU Catania - sede centrale\nSede presso Via Etnea, 570\nTel. 095/7517940 (ore 9:00/12:00)\nEmail: urp@ersucatania.gov.it\n\nOrari:\nLunedì: 09:00 - 12:00\nMercoledì: 15:30 - 18:00\nVenerdì: 09:00 - 12:00'
 		elif (text == '/ufficioersu' or text == '/ufficioersu@dmi_bot'):
-			messageText = 'ERSU Catania - Ufficio Tesserini\nSede della Cittadella (accanto l ingresso della Casa dello Studente)\n\nOrari:\nLunedi\': 09:00 - 12:30\nMartedi\': 09:00 - 12:30\nMercoledi\': 09:00 - 12:30 & 15:30 - 18:00\nGiovedi\': 09:00 - 12:30\nVenerdi\': 09:00 - 12:30'
+			messageText = 'ERSU Catania - Ufficio Tesserini\nSede della Cittadella (accanto l\'ingresso della Casa dello Studente)\n\nOrari:\nLunedì: 09:00 - 12:30\nMartedì: 09:00 - 12:30\nMercoledì: 09:00 - 12:30 & 15:30 - 18:00\nGiovedì: 09:00 - 12:30\nVenerdì: 09:00 - 12:30'
 		elif (text == '/urp' or text == '/urp@dmi_bot'):
 			messageText = 'URP Studenti\nSede in Via A.di Sangiuliano, 44\n\nTel. 800894327 (da fisso), 095 6139202/1/0\nEmail: urp-studenti@unict.it'
 		elif ('/professori' in text or '/professori@dmi_bot' in text or '/prof' in text or '/professore' in text or '/docente' in text or '/docenti' in text):
