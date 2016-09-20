c49eaedd3fd3d10ddc23957fcbcaf8218f376d7c
Veeenz <0xvinz@gmail.com>
2016-09-20 10:50:47 +0200

    Added the 'forum' function. It needs to be improved

--- telegrambot.py
+++ telegrambot.py
@@ -193,7 +193,13 @@ while True:
 				open('logs/log.txt', 'w').write(chat_ids)
 			else:
 				messageText = "News già abilitate!"
-
+		elif ('/forum' in text):
+			text = text.replace("/forum ","")
+			
+			dictUrlSezioni = forum(text)
+			if not (dictUrlSezioni == False):
+				for titoli in dictUrlSezioni:	
+					messageText = titoli+": "+str(dictUrlSezioni[x])
 
 	if messageText != "":
 		if logs != 0:
